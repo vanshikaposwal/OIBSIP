5bd2bc349dcd6bdfa775b5bf65944951ea7c85d2
vanshikaposwal <228863839+vanshikaposwal@users.noreply.github.com>
2026-09-05 19:06:11 +0530

    PNR Number generator created

--- JavaDev-Task1-OnlineReservationSystem/src/main/java/com/vanshika/oibsip/reservation/Main.java
+++ JavaDev-Task1-OnlineReservationSystem/src/main/java/com/vanshika/oibsip/reservation/Main.java
@@ -1,7 +1,9 @@
 package com.vanshika.oibsip.reservation;
 
+import static com.vanshika.oibsip.reservation.util.PNRGenerator.getPNR;
+
 public class Main {
     public static void main(String[] args){
-
+        System.out.println(getPNR());
     }
 }
--- JavaDev-Task1-OnlineReservationSystem/src/main/java/com/vanshika/oibsip/reservation/util/PNRGenerator.java
+++ JavaDev-Task1-OnlineReservationSystem/src/main/java/com/vanshika/oibsip/reservation/util/PNRGenerator.java
@@ -1,4 +1,12 @@
 package com.vanshika.oibsip.reservation.util;
 
 public class PNRGenerator {
+
+    public static String getPNR() {
+
+        long pnrNumber = 1000000000L +
+                        (long) (Math.random() * 9000000000L);
+
+        return "PNR" + pnrNumber;
+    }
 }
